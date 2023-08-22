5107050ee6ff5c0cfbba268f0fb213931552dd6a
subedisagar12 <subedisagar12@gmail.com>
2023-08-22 19:19:36 +0545

    JWT token

--- express/mini_project/routes/productRoutes.js
+++ express/mini_project/routes/productRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
 
 const productRoutes = express.Router();
 
@@ -6,16 +7,29 @@ const Product = require("../models/productModel");
 
 // Create product
 productRoutes.post("/create", async (req, res) => {
-  let newProduct = await Product.create({
-    name: req.body.name,
-    price: req.body.price,
-    image: req.body.image,
-    discount_price: req.body.discount_price,
-    is_featured: req.body.is_featured,
-    tags: req.body.tags,
-  });
+  try {
+    let token = req.headers.authorization.split(" ")[1];
 
-  res.json({ message: "Product Created", data: newProduct });
+    let tokenResponse = jwt.verify(
+      token,
+      "kjdfhgkjdhfgd@fkjgklfjg&fdgnjdfhgdfjg#fgng$"
+    );
+
+    console.log(tokenResponse);
+
+    let newProduct = await Product.create({
+      name: req.body.name,
+      price: req.body.price,
+      image: req.body.image,
+      discount_price: req.body.discount_price,
+      is_featured: req.body.is_featured,
+      tags: req.body.tags,
+    });
+
+    res.json({ message: "Product Created", data: newProduct });
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
 });
 
 // Get All Product
@@ -25,11 +39,21 @@ productRoutes.post("/create", async (req, res) => {
 // Operator -> gt,gte,lt,lte,eq,ne,in, nin
 // Logical Operator -> OR, AND, NOR, NOT
 productRoutes.get("/all", async (req, res) => {
-  let allProducts = await Product.find();
-  res.json({
-    message: "All product fetched",
-    data: allProducts,
-  });
+  try {
+    let token = req.headers.authorization?.split(" ")[1];
+
+    let tokenResponse = jwt.verify(
+      token,
+      "kjdfhgkjdhfgd@fkjgklfjg&fdgnjdfhgdfjg#fgng$"
+    );
+    let allProducts = await Product.find();
+    res.json({
+      message: "All product fetched",
+      data: allProducts,
+    });
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
 });
 
 // Get Single
